1b0f2dfd0006f52be7f3aba92d8292a7f6a07caf
namelesshacker <32227955+namelesshacker@users.noreply.github.com>
2024-05-22 11:27:53 +0200

    Update jsClient.js

--- jsClient.js
+++ jsClient.js
@@ -1,4 +1,30 @@
-var client = new Paho.MQTT.Client("[url of pi]", Number(9001), "clientId" + Date.now().toString());
+var client = new Paho.MQTT.Client("test.mosquitto.org", Number(8081),"/wss");
+//var subclient = new Paho.MQTT.Client("test.mosquitto.org", Number(8081),"/wss");
+//var pubclient = new Paho.MQTT.Client("test.mosquitto.org", Number(8081),"/wss");
+//var client = new Paho.MQTT.Client(location.hostname, Number(location.port), "clientId");
+
+
+
+//// connection option
+//const options = {
+//  		clean: true, // retain session
+//      connectTimeout: 4000, // Timeout period
+//      // Authentication information
+//      clientId: 'emqx_test',
+//      username: 'emqx_test',
+//      password: 'emqx_test',
+//}
+//
+//// Connect string, and specify the connection method by the protocol
+//// ws Unencrypted WebSocket connection
+//// wss Encrypted WebSocket connection
+//// mqtt Unencrypted TCP connection
+//// mqtts Encrypted TCP connection
+//// wxs WeChat applet connection
+//// alis Alipay applet connection
+//const connectUrl = 'wss://broker.emqx.io:8084/mqtt'
+//const client = mqtt.connect(connectUrl, options)
+
 
 // set callback handlers
 client.onConnectionLost = onConnectionLost;
@@ -13,7 +39,7 @@ var connect_options = {
     onSuccess: function () {
         // Connection succeeded; subscribe to our topic
         console.log('Connected!');
-        client.subscribe('uicomm/p-j', {qos: 0});
+        client.subscribe('env/test/TEMPERATURE', {qos: 0});
     },
     onFailure: function (message) {
         alert("Connection failed: " + message.errorMessage);
@@ -33,5 +59,5 @@ function onConnectionLost(responseObject) {
 // called when a message arrives
 function onMessageArrived(message) {
     console.log("Message Arrived:"+message.payloadString);
-    $("#msg_txt").html(message.payloadString);      
+    $("#msg_txt").html(message.payloadString);
 }
